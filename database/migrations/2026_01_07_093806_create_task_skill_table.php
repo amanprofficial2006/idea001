ad5fb1f94c4266a9058280c06d09ab8a5d878f9c
amanprofficial2006 <aman.profficial@gmail.com>
2026-01-07 16:03:58 +0530

    task api

--- database/migrations/2026_01_07_093806_create_task_skill_table.php
+++ database/migrations/2026_01_07_093806_create_task_skill_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('task_skill', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('task_id')->constrained('tasks');
+            $table->string('skill', 100);
             $table->timestamps();
         });
     }
