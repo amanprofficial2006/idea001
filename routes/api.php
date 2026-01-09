b720d130a441b1cd94fcd75c7f94b37f12f93afe
amanprofficial2006 <aman.profficial@gmail.com>
2026-01-09 12:34:17 +0530

    task api

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/profile', [UserController::class, 'updateProfile']);
     Route::post('/toggle-online', [UserController::class, 'toggleOnlineStatus']);
     Route::post('/tasks', [TaskController::class, 'store']);
-    Route::Post('/tasks/{id}', [TaskController::class, 'destroy']);
+    Route::Post('/delete', [TaskController::class, 'destroy']);
     Route::get('/task/{id}', [TaskController::class, 'show']);
     Route::get('/post-task', [TaskController::class, 'postedTasks']);
 });
