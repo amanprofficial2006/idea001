cd011a868d966d8ddee8cb7d4b0985eaf991204b
amanprofficial2006 <aman.profficial@gmail.com>
2026-01-19 21:04:20 +0530

    jo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 import './bootstrap';
+import { getDeviceToken } from './firebase';
+
+window.addEventListener('load', async () => {
+    const token = await getDeviceToken();
+    if (token) {
+        const deviceTokenInput = document.getElementById('device_token');
+        if (deviceTokenInput) {
+            deviceTokenInput.value = token;
+        }
+    }
+});
